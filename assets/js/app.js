15738d5b44e8d9ec605146f7f613d08c6954b975
OM-Coder95 <ommore8855@gmail.com>
2026-09-05 20:59:21 +0530

    implement read functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,74 @@ const backdrop = document.getElementById("backdrop");
 const movieModal = document.getElementById("movieModal");
 const toggleIcon = document.getElementById("toggleIcon");
 const hideMovieModalBtn = document.getElementById("hideMovieModalBtn");
+const movieContainer = document.getElementById("movieContainer");
 
 // DataBase
 
-let movieArr = movieArray1;
+let jsonArr = localStorage.getItem("movieArr");
 
-localStorage.setItem("movieArr", JSON.stringify(movieArr));
+let movieArr = jsonArr ? JSON.parse(jsonArr) : [];
+
+cl(movieArr);
 
 // Functions
+
+// Toggling Function
 
 function onAddMovieBtnClick() {
   backdrop.classList.toggle("active");
   movieModal.classList.toggle("active");
 }
+
+// setRating
+
+function setRating(rating) {
+    if(rating > 7) {
+        return "badge-success"
+    } else if (rating > 5) {
+        return "badge-warning"
+    } else {
+        return "badge-danger"
+    }
+}
+
+// Read
+
+function showOnUI(arr) {
+  let result = "";
+
+  arr.forEach((ele) => {
+    result += `
+               <div class="col-md-3 mb-3">
+                <div class="card movieCard" id="${ele.id}">
+                 
+                    <div class="card-header d-flex justify-content-between cardHeading">
+                        <h4 class="m-0">${ele.original_title}</h4>
+                        <h5 class="m-0"><span class="badge ${setRating(ele.vote_average)}">${ele.vote_average}</span></h5>
+                    </div>
+                   
+                    <div class="card-body py-0">
+                        <figure class="m-0">
+                            <img src="https://image.tmdb.org/t/p/w500${ele.poster_path || ele.backdrop_path}" alt="${ele.original_title}" title="${ele.original_title}">
+                            <figcaption>
+                                <h4 class="m-0">${ele.original_title}</h4>
+                                <p class="m-0">${ele.overview}</p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                
+                    <div class="card-footer d-flex justify-content-between">
+                        <button class="btn btn-sm net-pri-btn">Edit</button>
+                        <button class="btn btn-sm net-sec-btn">Remove</button>
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+  movieContainer.innerHTML = result;
+}
+
+showOnUI(movieArr)
 
 toggleBtn.addEventListener("click", onAddMovieBtnClick);
 toggleIcon.addEventListener("click", onAddMovieBtnClick);
