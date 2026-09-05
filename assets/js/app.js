6a6f659060c9020bc81555225752ca2646728629
OM-Coder95 <ommore8855@gmail.com>
2026-09-05 22:44:06 +0530

    Implement create Functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,11 @@ const movieModal = document.getElementById("movieModal");
 const toggleIcon = document.getElementById("toggleIcon");
 const hideMovieModalBtn = document.getElementById("hideMovieModalBtn");
 const movieContainer = document.getElementById("movieContainer");
+const form = document.getElementById("form");
+const movieName = document.getElementById("movieName");
+const movieImage = document.getElementById("movieImage");
+const movieDescription = document.getElementById("movieDescription");
+const movieRating = document.getElementById("movieRating");
 
 // DataBase
 
@@ -24,16 +29,22 @@ function onAddMovieBtnClick() {
   movieModal.classList.toggle("active");
 }
 
+// SaveData
+
+function saveDataInLS() {
+  localStorage.setItem("movieArr", JSON.stringify(movieArr));
+}
+
 // setRating
 
 function setRating(rating) {
-    if(rating > 7) {
-        return "badge-success"
-    } else if (rating > 5) {
-        return "badge-warning"
-    } else {
-        return "badge-danger"
-    }
+  if (rating > 7) {
+    return "badge-success";
+  } else if (rating > 5) {
+    return "badge-warning";
+  } else {
+    return "badge-danger";
+  }
 }
 
 // Read
@@ -72,9 +83,69 @@ function showOnUI(arr) {
   movieContainer.innerHTML = result;
 }
 
-showOnUI(movieArr)
+showOnUI(movieArr);
+
+// create
+
+function onMovieAdd(event) {
+  event.preventDefault();
+
+  let newMovie = {
+    id: crypto.randomUUID(),
+    original_title: movieName.value.trim(),
+    poster_path: movieImage.value.trim(),
+    overview: movieDescription.value.trim(),
+    vote_average: movieRating.value,
+  };
+
+  movieArr.unshift(newMovie);
+  saveDataInLS();
+  Swal.fire({
+    title: "Movie Added!",
+    text: "Movie card created successfully.",
+    icon: "success",
+    timer: 2000,
+  });
+  form.reset();
+  onAddMovieBtnClick();
+
+  //   UI
+
+  let div = document.createElement("div");
+
+  div.id = newMovie.id;
+
+  div.className = `col-md-3 mb-3`;
+
+  div.innerHTML = `
+                  <div class="card movieCard" id="${newMovie.id}">
+                 
+                    <div class="card-header d-flex justify-content-between cardHeading">
+                        <h4 class="m-0">${newMovie.original_title}</h4>
+                        <h5 class="m-0"><span class="badge ${setRating(newMovie.vote_average)}">${newMovie.vote_average}</span></h5>
+                    </div>
+                   
+                    <div class="card-body py-0">
+                        <figure class="m-0">
+                            <img src="https://image.tmdb.org/t/p/w500${newMovie.poster_path || newMovie.backdrop_path}" alt="${newMovie.original_title}" title="${newMovie.original_title}">
+                            <figcaption>
+                                <h4 class="m-0">${newMovie.original_title}</h4>
+                                <p class="m-0">${newMovie.overview}</p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                
+                    <div class="card-footer d-flex justify-content-between">
+                        <button class="btn btn-sm net-pri-btn">Edit</button>
+                        <button class="btn btn-sm net-sec-btn">Remove</button>
+                    </div>
+                </div>
+  `;
+  movieContainer.prepend(div);
+}
 
 toggleBtn.addEventListener("click", onAddMovieBtnClick);
 toggleIcon.addEventListener("click", onAddMovieBtnClick);
 hideMovieModalBtn.addEventListener("click", onAddMovieBtnClick);
 backdrop.addEventListener("click", onAddMovieBtnClick);
+form.addEventListener("submit", onMovieAdd);
